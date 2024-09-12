13b517abfcc66d67bfdd7e0ed8c9aeb344ad0d09
Sallein <saleinkyl@gmail.com>
2024-09-12 23:04:19 +0300

    Добавление динамических метаданных

--- next/app/about/page.js
+++ next/app/about/page.js
@@ -1,3 +1,8 @@
+export const metadata = {
+    title: "About Page",
+    description: "About Page",
+}
+
 export default function Home() {
     return (
     <h1>About Page</h1>
--- next/app/post/[id]/page.js
+++ next/app/post/[id]/page.js
@@ -1,5 +1,13 @@
 import Post from '@/app/components/Post'
 
+export async function generateMetadata({params, searchParams}) {
+    const post = await fetchData(params.id)
+    return {
+        title: post.title + ` - Статья на сайте`,
+        description: post.body
+    }
+}
+
 async function fetchData(id) {
     const response = await fetch('https://jsonplaceholder.typicode.com/posts/' + id)
     const result = await response.json()
