cc25c8231325eb76d3fbbe5e8a41c9da58a35b68
Sallein <saleinkyl@gmail.com>
2024-09-12 20:42:49 +0300

    отслеживание URL

--- next/app/page.js
+++ next/app/page.js
@@ -1,6 +1,24 @@
+import Link from "next/link"
 
-export default function Home() {
+async function fetchData() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const result = await response.json()
+  return result
+}
+
+export default async function Home() {
+  const posts = await fetchData()
+
   return (
+    <div>
     <h1>Main Page</h1>
+    {posts.map(el => (
+      <div key={el.id} className="post">
+        <h2>{el.title}</h2>
+        <p>{el.body}</p>
+        <Link href={`/post/` + el.id}>Details...</Link>
+      </div>
+    ))}
+    </div>
   );
 }
